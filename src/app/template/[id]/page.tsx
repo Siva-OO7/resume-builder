43c3ec4635bc9db337bf4467b9050026128354a7
Siva-OO7 <sivaramakrishnmag253@gmail.com>
2025-03-27 01:57:56 +0530

    Pageprops issue resolved

--- src/app/template/[id]/page.tsx
+++ src/app/template/[id]/page.tsx
@@ -1,23 +1,48 @@
+// import { getTemplateById } from '@/server-actions/templates'
+// import React from 'react'
+// import Resume from '../_components/resume'
+
+
+// async function TemplatePreview( {params} : {
+//     params : {
+//         id: string;
+//     };
+//   }
+// ) {
+
+//   const {id} = params;
+
+//     const response = await getTemplateById(id);
+//   return (
+//     <div>
+//       <Resume template={response.data} />
+//     </div>
+//   )
+// }
+
+// export default TemplatePreview
+
 import { getTemplateById } from '@/server-actions/templates'
 import React from 'react'
 import Resume from '../_components/resume'
 
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-async function TemplatePreview( {params} : {
-    params : {
-        id: string;
-    };
-  }
-) {
+async function TemplatePreview({ params }: PageProps) {
+  // params is already an object, no need to await
+  const { id } = params; 
 
-  const {id} = params;
-
-    const response = await getTemplateById(id);
+  const response = await getTemplateById(id);
+  
   return (
     <div>
       <Resume template={response.data} />
     </div>
-  )
+  );
 }
 
-export default TemplatePreview
+export default TemplatePreview;
